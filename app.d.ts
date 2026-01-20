fc5bf43bb741ad787dea5830208669d1e7d3fa55
LeandroPalma27 <psychomusicbox@gmail.com>
2026-01-20 16:02:32 -0500

    Inicio de curso de ts

---
+++ app.d.ts
@@ -0,0 +1,2 @@
+declare const msg: string;
+//# sourceMappingURL=app.d.ts.map
---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+var msg = "Hola mundo";
+var hero = {
+    name: "Ironman",
+    age: 45
+};
+hero.age = 50;
+console.log(hero.age + 1);
